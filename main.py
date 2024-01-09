c1ff843f5e888e0ce3058011a2843ccaf39b99b5
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-01-09 16:16:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ bot = Client("my_bot", api_id=3845818, api_hash="95937bcf6bc0938f263fc7ad96959c6
 def search_anime(query):
     url = f"https://anidl.org/wp-json/wp/v2/search?search={query}"
     response = requests.get(url)
-    result = response.json()
-    return result
+    if response.ok:
+        try:
+            result = response.json()
+            return result
+        except ValueError:
+            print("Invalid JSON")
+    else:
+        print("Response not OK")
 
 # Define a command handler to handle user search queries
 @bot.on_message(filters.command("search"))
